ef797004012ab3dd58ab95dd1e0b8e6f703aad83
Gabryan1995 <32003034+Gabryan1995@users.noreply.github.com>
2017-10-04 20:31:53 -0500

    Basic Physics
    Added PhysicsObject class and PlayerController class with basic physics and Camera that follows the player.

---
+++ ACMP_2D_Game/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : PhysicsObject {
+
+    public float maxSpeed = 6;
+    public float jumpTakeOffSpeed = 6;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    protected override void ComputeVelocity()
+    {
+        Vector2 move = Vector2.zero;
+
+        move.x = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && grounded) {
+            velocity.y = jumpTakeOffSpeed;
+        }
+        else if (Input.GetButtonUp("Jump")) {
+            if (velocity.y > 0)
+                velocity.y = velocity.y * .5f;
+        }
+
+        targetVelocity = move * maxSpeed;
+    }
+}
