afb6570a4f8333297326ab881de26bb695eba378
Jacob Liou <jacob.c.liou@gmail.com>
2017-10-18 21:42:13 -0500

    Adding road roller hitbox

---
+++ ACMP_2D_Game/Assets/Scripts/AttackObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackObject : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        yield return new WaitForSeconds(0.5f);
+        this.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+        this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+}
