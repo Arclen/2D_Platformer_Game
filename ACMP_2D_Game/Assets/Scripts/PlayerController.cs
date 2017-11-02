b1aa76f391cce8a5b50c8e6b639ff4178fb316f2
Jacob Liou <jacob.c.liou@gmail.com>
2017-11-01 19:26:04 -0500

    Adding projectiles to character

--- ACMP_2D_Game/Assets/Scripts/PlayerController.cs
+++ ACMP_2D_Game/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : PhysicsObject {
 
     public float maxSpeed = 6;
     public float jumpTakeOffSpeed = 6;
+
+    private List<GameObject> Projectiles = new List<GameObject>();
+    public GameObject projectilePrefab;
+    private float projectileVelocity;
     
 	// Use this for initialization
 	void Start () {
-		
+        projectileVelocity = 3;
 	}
 
     protected override void ComputeVelocity()
@@ -17,7 +21,10 @@ public class PlayerController : PhysicsObject {
         Vector2 move = Vector2.zero;
 
         move.x = Input.GetAxis("Horizontal");
-
+        if (move.x > 0)
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        if(move.x < 0)
+            this.gameObject.GetComponent<SpriteRenderer>().flipX = true;
         if (Input.GetButtonDown("Jump") && grounded) {
             velocity.y = jumpTakeOffSpeed;
         }
@@ -27,5 +34,34 @@ public class PlayerController : PhysicsObject {
         }
 
         targetVelocity = move * maxSpeed;
+    }
+
+    private void Update()
+    {
+        targetVelocity = Vector2.zero;
+        ComputeVelocity();
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            GameObject bullet = (GameObject)Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Projectiles.Add(bullet);
+        }
+        //bool trueForLeft = (this.gameObject.GetComponent<SpriteRenderer>().flipX == true);
+        for (int i=0; i < Projectiles.Count; i++)
+        {
+            GameObject goBullet = Projectiles[i];
+            if (goBullet != null)
+            {
+            //    if(trueForLeft)
+                    goBullet.transform.Translate(new Vector3(2, 0) * Time.deltaTime * projectileVelocity);
+              //  else goBullet.transform.Translate(new Vector3(-2, 0) * Time.deltaTime * projectileVelocity);
+
+                Vector3 bulletScreenPos = Camera.main.WorldToScreenPoint(goBullet.transform.position);
+                if(bulletScreenPos.x >= Screen.width || bulletScreenPos.x <= 0)
+                {
+                    DestroyObject(goBullet);
+                    Projectiles.Remove(goBullet);
+                }
+            }
+        }
     }
 }
